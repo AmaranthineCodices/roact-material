e32a2b8b6452e5b5433ca9574d2a8f9fa24a85d9
ZachCurtis <31259055+ZachCurtis@users.noreply.github.com>
2018-03-13 08:32:21 -0400

    animation improvement

--- src/Components/Switch.lua
+++ src/Components/Switch.lua
@@ -40,8 +40,8 @@ function Switch:willUpdate(nextProps, nextState)
                 RoactAnimate.Prepare(self.state._rippleSize, UDim2.new(0, 0, 0, 0)),
                 RoactAnimate.Prepare(self.state._rippleTransparency, 0.6),
                 RoactAnimate.Prepare(self.state._rippleColor, newRippleColor),
-                RoactAnimate(self.state._rippleSize, TweenInfo.new(0.15), UDim2.new(1.75, 0, 1.75, 0)),
-                RoactAnimate(self.state._rippleTransparency, TweenInfo.new(0.15), 1)
+                RoactAnimate(self.state._rippleSize, TweenInfo.new(0.25, Enum.EasingStyle.Sine), UDim2.new(1.75, 0, 1.75, 0)),
+                RoactAnimate(self.state._rippleTransparency, TweenInfo.new(.08, Enum.EasingStyle.Quart, Enum.EasingDirection.InOut), 1)
             })
         }):Start()
     end
